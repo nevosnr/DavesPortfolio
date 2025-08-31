958cd0f8fa299e51e5ffa6372be54143994e9961
Dave Neve <64417697+nevosnr@users.noreply.github.com>
2025-08-31 13:07:00 +0100

    Added MudBlazor Dependancies

--- DavesPortfolio.Client/Program.cs
+++ DavesPortfolio.Client/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
