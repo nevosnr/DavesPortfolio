73e1909411dea33331221bf08fa8abb8cd7f7dc1
Dave Neve <64417697+nevosnr@users.noreply.github.com>
2025-08-31 22:25:49 +0100

    Created the start of the a/c recce.

---
+++ DavesPortfolio.Client/Pages/Recognise.razor.cs
@@ -0,0 +1,14 @@
+using MudBlazor;
+
+namespace DavesPortfolio.Client.Pages
+{
+    public partial class Recognise
+    {
+        int result = 0;
+
+        private int AddResult()
+        {
+            return result++;
+        }
+    }
+}
