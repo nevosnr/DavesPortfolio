ef74f1692ceec0faa499f58d07e71b85147e3cee
Dave Neve <64417697+nevosnr@users.noreply.github.com>
2025-09-03 15:52:44 +0100

    minor updates to components

--- DavesPortfolio.Client/Pages/Layout.razor.cs
+++ DavesPortfolio.Client/Pages/Layout.razor.cs
@@ -2,6 +2,20 @@ namespace DavesPortfolio.Client.Pages
 {
     public partial class Layout
     {
+        public record Load(string name, string position);
+        public IEnumerable<Load> loadout;
 
+        protected override void OnInitialized()
+        {
+            loadout = new List<Load>
+            {
+                new Load("Weapon_1", "station_5"),
+                new Load("Weapon_2", "station_4"),
+                new Load("Weapon_3", "station_3"),
+                new Load("Weapon_4", "station_2"),
+                new Load("Weapon_5", "station_1"),
+
+            };
+        }
     }
 }
