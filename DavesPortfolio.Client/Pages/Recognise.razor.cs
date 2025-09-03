540c9649ec45ea357d682f393e59f9b1bc6ecf5a
Dave Neve <64417697+nevosnr@users.noreply.github.com>
2025-09-03 22:24:04 +0100

    basics of the module have been completed.

--- DavesPortfolio.Client/Pages/Recognise.razor.cs
+++ DavesPortfolio.Client/Pages/Recognise.razor.cs
@@ -4,11 +4,30 @@ namespace DavesPortfolio.Client.Pages
 {
     public partial class Recognise
     {
+        int calledResult = 0;
         int result = 0;
 
-        private int AddResult()
+        private void AddResult()
         {
-            return result++;
+            result++;
+        }
+
+        private void ResetResult()
+        {
+            Console.WriteLine("I've reset!");
+            result = 0;
+            calledResult = 0;
+        }
+
+        private int ReturnResult()
+        {
+            Console.WriteLine(result);
+            return result;
+        }
+
+        private void CallResult()
+        {
+            calledResult = ReturnResult();
         }
     }
 }
