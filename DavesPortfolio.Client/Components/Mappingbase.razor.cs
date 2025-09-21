0b13d681b8799c4ce8133a2730d884d2f0076f55
Dave Neve <64417697+nevosnr@users.noreply.github.com>
2025-09-21 16:23:58 +0100

    Created new map component using JS Interop and removed the LeafletForBlazor library as it was too bloated for the application.

--- DavesPortfolio.Client/Components/Mappingbase.razor.cs
+++ DavesPortfolio.Client/Components/Mappingbase.razor.cs
@@ -1,17 +1,24 @@
-using LeafletForBlazor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Runtime.CompilerServices;
 
 namespace DavesPortfolio.Client.Components
 {
-    public partial class Mappingbase
+    public partial class Mappingbase : ComponentBase
     {
-        RealTimeMap.LoadParameters parameters = new RealTimeMap.LoadParameters()
+        [Parameter] public double Lat { get; set; } = 51.505;
+        [Parameter] public double Lng { get; set; } = -0.09;
+        [Parameter] public int Zoom { get; set; } = 13;
+        [Parameter] public string mapId { get; set; } = $"map-{Guid.NewGuid()}";
+
+        [Inject] IJSRuntime JS { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            location = new RealTimeMap.Location()
+            if (firstRender && JS != null)
             {
-                latitude = 51.5074,
-                longitude = -0.1278
-            },
-            zoomLevel = 13
-        };
+                await JS.InvokeVoidAsync("initMap", mapId, Lat, Lng, Zoom);
+            }
+        }
     }
 }
---
+++ DavesPortfolio.Client/wwwroot/js/map.js
@@ -0,0 +1,7 @@
+﻿window.initMap = (mapId, latitude, longitude, zoom) => {
+    const map = L.map(mapId).setView([latitude, longitude], zoom);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attributation: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+};
