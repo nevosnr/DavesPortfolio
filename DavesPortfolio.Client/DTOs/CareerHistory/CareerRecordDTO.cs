ca5d8b514d8cfd54a97f3a612322d01b4091ea70
Dave Neve <64417697+nevosnr@users.noreply.github.com>
2025-10-12 16:43:48 +0100

    Created DTO for career card in prep for Db operations.

---
+++ DavesPortfolio.Client/DTOs/CareerHistory/CareerRecordDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DavesPortfolio.Client.DTOs.CareerHistory
+{
+    public record CareerRecordDTO
+    (
+        string? _careerIcon,
+        string? _careerJobTitle,
+        string? _careerRoleTitle,
+        string? _careerShortDescription,
+        string? _careerLongDescription
+
+    );
+}
+
+` 
